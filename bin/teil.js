5a934bbf699211134e024d01986eef2bd2da19e4
Sebastian Senf <contact@mustardamus.com>
2017-10-26 18:43:57 +0200

    ability to pass a custom config file to bin

--- bin/teil.js
+++ bin/teil.js
@@ -1,17 +1,32 @@
 #!/usr/bin/env node
 
+const { join } = require('path')
 const zargs = require('yargs')
 const server = require('../lib/server')
 
+const getConfigFilePath = argv => {
+  let configFile
+
+  if (argv['config-file']) {
+    configFile = join(process.cwd(), argv['config-file'])
+  }
+
+  return configFile
+}
+
 const app = zargs
   .usage('$0 <cmd> [args]')
-  .command(['dev', '$0'], 'Start development server', yargs => {
-    server.start()
+  .command(['dev', '$0'], 'Start development server', yargs => {}, argv => {
+    const configFile = getConfigFilePath(argv)
+    server.start(configFile)
   })
-  .command('start', 'Start the production server', yargs => {
+  .command('start', 'Start the production server', yargs => {}, argv => {
     process.env.NODE_ENV = 'production'
-    server.start()
+
+    const configFile = getConfigFilePath(argv)
+    server.start(configFile)
   })
+  .option('config-file', { alias: 'c' })
   .help()
 
 /* eslint-disable */
