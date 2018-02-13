a66633b5411517e82bac4eadfbfc3b2a49b29817
Sebastian Senf <contact@mustardamus.com>
2018-02-13 21:00:16 +0100

    ability to use as middleware and no listening server, simple integration as nuxt.js module

---
+++ index.js
@@ -0,0 +1,14 @@
+const { join } = require('path')
+const middleware = require('./lib/middleware')
+
+module.exports = async function (moduleOptions) {
+  if (!this.nuxt) {
+    throw new Error('Only available as Nuxt module')
+  }
+
+  const cwd = process.cwd()
+  const configFile = join(cwd, 'teil.config.js')
+  const handler = await middleware(configFile)
+
+  this.addServerMiddleware({ path: '/', handler })
+}
