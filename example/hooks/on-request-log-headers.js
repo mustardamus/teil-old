60b2919c721ccccc7936b8bc2a6d91322038dedc
Sebastian Senf <contact@mustardamus.com>
2018-02-15 23:16:31 +0100

    removed hooks from example as they are not used right now

--- example/hooks/on-request-log-headers.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req, res, next) => {
-  console.log('onRequest Hook Headers Log:', req.headers)
-  next()
-}
--- example/hooks/on-send-log-payload.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-  (request, response, payload, next) => {
-    console.log('onSend Hook Log Payload', payload)
-    next()
-  }
-]
