75845c38f6d66c59e6b4deb5e3138de6b905d37c
Sebastian Senf <contact@mustardamus.com>
2018-05-16 12:23:03 +0200

    pass down mongoose validation errors in the error handling middleware, tests for that

--- tests/middleware-error-handling.test.js
+++ tests/middleware-error-handling.test.js
@@ -1,10 +1,72 @@
 const middlewareErrorHandling = require('../lib/middleware-error-handling')
 
+const middleware = middlewareErrorHandling()
+
 describe('Middleware Error Handling', () => {
   it('should export a error handling middleware function', () => {
-    const middleware = middlewareErrorHandling()
-
     expect(typeof middleware).toBe('function')
     expect(middleware.length).toBe(4)
+  })
+
+  it('should call next without anything if no error was passed', () => {
+    const next = jest.fn()
+
+    middleware(null, null, null, next)
+
+    expect(next.mock.calls).toHaveLength(1)
+    expect(next.mock.calls[0]).toHaveLength(0)
+  })
+
+  it('should send back a general error message', () => {
+    const err = { message: 'general error' }
+    const req = { method: 'POST', route: { path: '/' } }
+    const status = jest.fn()
+    const send = jest.fn()
+    const res = {
+      status (statusCode) {
+        status(statusCode)
+        return { send }
+      }
+    }
+    const next = jest.fn()
+
+    middleware(err, req, res, next)
+
+    expect(status.mock.calls).toHaveLength(1)
+    expect(status.mock.calls[0][0]).toBe(500)
+    expect(send.mock.calls).toHaveLength(1)
+    expect(send.mock.calls[0][0]).toEqual(err)
+    expect(next.mock.calls).toHaveLength(1)
+    expect(next.mock.calls[0][0].includes('ERR!')).toBe(true)
+  })
+
+  it('should send back a extended error message if mongoose validation error', () => {
+    const err = {
+      message: 'Validation failed',
+      errors: {
+        email: {
+          message: 'Not unique'
+        }
+      }
+    }
+    const req = { method: 'POST', route: { path: '/' } }
+    const status = jest.fn()
+    const send = jest.fn()
+    const res = {
+      status (statusCode) {
+        status(statusCode)
+        return { send }
+      }
+    }
+    const next = jest.fn()
+
+    middleware(err, req, res, next)
+
+    expect(status.mock.calls).toHaveLength(1)
+    expect(status.mock.calls[0][0]).toBe(500)
+    expect(send.mock.calls).toHaveLength(1)
+    expect(send.mock.calls[0][0]).toEqual(err)
+    expect(next.mock.calls).toHaveLength(1)
+    expect(next.mock.calls[0][0].includes('ERR!')).toBe(true)
   })
 })
